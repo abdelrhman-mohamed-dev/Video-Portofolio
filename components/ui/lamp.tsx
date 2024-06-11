c01914912c77d86629ed47878c5068a9452a397f
Abdelrhman Mohamed <81966035+abdelrhman-mohamed-dev@users.noreply.github.com>
2024-06-11 14:18:07 +0300

    Update lamp.tsx
    change text decoration

--- components/ui/lamp.tsx
+++ components/ui/lamp.tsx
@@ -24,7 +24,7 @@ export function LampDemo() {
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
         Crafting Visual Magic Daily <br /> 
-        @Younis
+        Younis
       </motion.h1>
     </LampContainer>
   );
